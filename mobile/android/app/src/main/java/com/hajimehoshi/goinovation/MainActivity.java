a9bd650ade2a35863df7fce335264f859c573fdd
Hajime Hoshi <hajimehoshi@gmail.com>
2019-08-18 21:43:25 +0900

    Remove unused imports

--- mobile/android/app/src/main/java/com/hajimehoshi/goinovation/MainActivity.java
+++ mobile/android/app/src/main/java/com/hajimehoshi/goinovation/MainActivity.java
@@ -1,10 +1,7 @@
 package com.hajimehoshi.goinovation;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.util.Log;
 
 import go.Seq;
 
