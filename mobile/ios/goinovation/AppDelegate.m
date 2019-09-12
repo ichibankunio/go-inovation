08008c63c8dda7fa83a021c12e4fd15b504fb46d
Hajime Hoshi <hajimehoshi@gmail.com>
2019-09-13 03:23:11 +0900

    mobile: Suspend/resume game

--- mobile/ios/goinovation/AppDelegate.m
+++ mobile/ios/goinovation/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "MobileEbitenViewControllerWithErrorHandling.h"
+
 @interface AppDelegate ()
 
 @end
@@ -22,6 +24,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [[self ebitenViewContrller] suspendGame];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -35,10 +38,16 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[self ebitenViewContrller] resumeGame];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (MobileEbitenViewControllerWithErrorHandling*)ebitenViewContrller
+{
+    return (MobileEbitenViewControllerWithErrorHandling*)([[self window] rootViewController]);
 }
 
 @end
