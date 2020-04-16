557767a5999e5705db0051f7244f4fbd6742ce4d
Hajime Hoshi <hajimehoshi@gmail.com>
2020-04-17 03:03:03 +0900

    Add null check for navigator
    We are adding a very special environment (MonoGame) that doesn't
    work Web APIs.
    
    See also hajimehoshi/ebiten#1078

--- ino/game_js.go
+++ ino/game_js.go
@@ -25,7 +25,11 @@ func tryLoseContext() bool {
 }
 
 func systemLang() language.Tag {
-	str := js.Global().Get("navigator").Get("language").String()
+	nav := js.Global().Get("navigator")
+	if nav.IsNull() {
+		return language.Japanese
+	}
+	str := nav.Get("language").String()
 	newLang, _ := language.Parse(str)
 	base, _ := newLang.Base()
 	newLang, _ = language.Compose(base)
