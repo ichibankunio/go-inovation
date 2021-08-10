9db15fd08694c1a6af452b4c14fcb83f2decafe2
Hajime Hoshi <hajimehoshi@gmail.com>
2021-08-11 00:07:22 +0900

    Create .app

--- steam_build.sh
+++ steam_build.sh
@@ -1,4 +1,5 @@
 name=innovation2007
+name_app=Innovation2007.app
 mkdir -p bin
 
 # Windows
@@ -6,5 +7,21 @@ mkdir -p bin
 GOOS=windows GOARCH=amd64 go build -tags=steam -o bin/${name}.exe .
 
 # macOS
-# TODO: Create .app
-go build -tags=steam -o bin/${name} .
+rm -rf bin/${name_app}
+mkdir -p bin/${name_app}/Contents/MacOS
+mkdir -p bin/${name_app}/Contents/Resources
+
+go build -tags=steam -o bin/${name_app}/Contents/MacOS/${name} .
+cp steam/icon/icon_512x512.icns bin/${name_app}/Contents/Resources/icon.icns
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>CFBundleExecutable</key>
+    <string>{{.Name}}</string>
+    <key>CFBundleIconFile</key>
+    <string>icon.icns</string>
+    <key>NSHighResolutionCapable</key>
+    <true />
+  </dict>
+</plist>' | sed -e "s/{{.Name}}/${name}/g" > bin/${name_app}/Contents/Info.plist
