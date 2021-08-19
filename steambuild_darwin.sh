30fe1a7f2bbed57f2713dbc7e6d2286630af1b5a
Hajime Hoshi <hajimehoshi@gmail.com>
2021-08-19 22:33:45 +0900

    Fix Info.plist

--- steambuild_darwin.sh
+++ steambuild_darwin.sh
@@ -14,12 +14,20 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
   <dict>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleInfoDictionaryVersion</key>
+    <string>6.0</string>
     <key>CFBundleExecutable</key>
     <string>{{.Name}}</string>
     <key>CFBundleIdentifier</key>
     <string>{{.BundleID}}</string>
     <key>CFBundleIconFile</key>
     <string>icon.icns</string>
+    <key>CFBundleVersion</key>
+    <string>0.0.0</string>
+    <key>CFBundleShortVersionString</key>
+    <string>0.0.0</string>
     <key>NSHighResolutionCapable</key>
     <true />
     <key>LSMinimumSystemVersion</key>
